50e1ebe19e911629258a0c74289e52c76199fab6
axlfrost <ale.gonzalezlevio@gmail.com>
2019-12-12 17:34:09 -0300

    Actualización inicio e implementación gráfico

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,6 +6,9 @@ import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { GraficoBarrasHorizontalComponent } from './grafico-barras-horizontal/grafico-barras-horizontal.component';
 
+// Modules
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,9 @@ import { GraficoBarrasHorizontalComponent } from './grafico-barras-horizontal/gr
     // Directivas propias de Angular
     CommonModule,
     // Navegación
-    RouterModule
+    RouterModule,
+    NgxChartsModule,
+    BrowserAnimationsModule
   ],
   // Para poder utilizarlo fuera
   exports: [
--- src/app/components/grafico-barras-horizontal/grafico-barras-horizontal.component.ts
+++ src/app/components/grafico-barras-horizontal/grafico-barras-horizontal.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-grafico-barras-horizontal',
   templateUrl: './grafico-barras-horizontal.component.html',
   styleUrls: ['./grafico-barras-horizontal.component.css']
 })
-export class GraficoBarrasHorizontalComponent implements OnInit {
+export class GraficoBarrasHorizontalComponent implements OnInit, OnDestroy {
 
-  constructor() { }
 
+  // Datos de prueba
+  public results: any[] = [
+    {
+      name: 'Juego1',
+      value: 500
+    },
+    {
+      name: 'Juego2',
+      value: 1212
+    },
+    {
+      name: 'Juego3',
+      value: 222
+    },
+    {
+      name: 'Juego4',
+      value: 1112
+    }
+  ];
+
+  // options
+  public showXAxis = true;
+  public showYAxis = true;
+  public gradient = true;
+  public showLegend = true;
+  public showXAxisLabel = true;
+  public xAxisLabel = 'Juegos';
+  public showYAxisLabel = true;
+  public yAxisLabel = 'Votos';
+  public colorScheme = 'nightLights';
+
+  public intervalo;
+
+  constructor() {
+    // Prueba de datos en "tiempo-real"
+    this.intervalo = setInterval(() => {
+      const newResult = [...this.results];
+
+      // tslint:disable-next-line: forin
+      for (const i in newResult) {
+        newResult[i].value = Math.round(Math.random() * 100);
+      }
+
+      this.results = [...newResult];
+      console.log('tick');
+    }, 1500);
+
+
+  }
+  ngOnDestroy(): void {
+    clearInterval(this.intervalo);
+  }
   ngOnInit() {
   }
+
+
+
+
+  public onSelect(event) {
+    console.log(event);
+  }
+
+
 
 }
